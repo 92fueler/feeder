0fe342db45deee6df160a53e27232649a3097cd0
Joyce Wu <40779030+92fueler@users.noreply.github.com>
2020-12-12 14:25:37 -0800

    Create service.py
    installed python-jsonrpc and created Python HTTP server with RequestHandler

---
+++ backend/service.py
@@ -0,0 +1,21 @@
+import pyjsonrpc
+
+SERVER_HOST = 'localhost'
+SERVER_PORT = 4040
+
+class RequestHandler(pyjsonrpc.HttpRequestHandler):
+    """ Test method """
+    # defining a RPC API is easy as below
+    @pyjsonrpc.rpcmethod
+    def add(self, a, b):
+        print "add is called with %d and %d" % (a, b)
+        return a + b
+
+http_server = pyjsonrpc.ThreadingHttpServer(
+    server_address = (SERVER_HOST, SERVER_PORT),
+    RequestHandlerClass = RequestHandler
+)
+
+print "Starting HTTP server on %s:%d" % (SERVER_HOST, SERVER_PORT)
+
+http_server.serve_forever()
